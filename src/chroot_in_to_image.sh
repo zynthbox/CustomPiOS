e40f839859259216f3d25c7332f4ac8c0f0ff79d
probal31 <probal31@gmail.com>
2026-04-24 17:07:12 +0530

    fix: Fix mount path specific for devuan

--- src/chroot_in_to_image.sh
+++ src/chroot_in_to_image.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "${DIR}"/common.sh
 pushd /distro/workspace
-    mount_image *.img 2 ./mount boot/firmware
+    mount_image *.img 2 ./mount boot/broadcom
     pushd mount
         chroot .
     popd
